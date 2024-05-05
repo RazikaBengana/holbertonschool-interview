104de7becc1d6d452f69be2695e1ef48672fbdf4
RazikaBengana <razikabengana@gmail.com>
2024-05-05 16:43:29 +0200

    Header file

---
+++ menger/menger.h
@@ -0,0 +1,14 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+
+#include <math.h>
+#include <stdio.h>
+
+
+/* 0-menger.c */
+void print_sponge(int level, int row, int col);
+void menger(int level);
+
+
+#endif /* MENGER_H */
