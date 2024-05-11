842c95a498000f5dabdfc68bff6aa8a87b8357b6
RazikaBengana <razikabengana@gmail.com>
2024-05-11 13:11:58 +0200

    File for testing

---
+++ double_circular_linked_list/test file/0-main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * print_list - Prints informations about each element of a list
+ *
+ * @list: Pointer to the head of the linkd list
+ *
+ * Return: nothing (void)
+ */
+
+void print_list(List *list)
+{
+	List *tmp;
+
+	tmp = list;
+
+	while (tmp)
+	{
+		printf("%s\n", tmp->str);
+		printf("\t->prev: %s\n", tmp->prev ? tmp->prev->str : "NULL");
+		printf("\t->next: %s\n", tmp->next ? tmp->next->str : "NULL");
+
+		tmp = tmp->next;
+
+		if (tmp == list)
+			break;
+	}
+}
+
+
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	List *list;
+
+	list = NULL;
+	add_node_end(&list, "Holberton");
+	add_node_end(&list, "School");
+	add_node_end(&list, "Full");
+	add_node_end(&list, "Stack");
+	add_node_end(&list, "Engineer");
+
+	printf("Added to the end:\n");
+	print_list(list);
+
+	list = NULL;
+	add_node_begin(&list, "Holberton");
+	add_node_begin(&list, "School");
+	add_node_begin(&list, "Full");
+	add_node_begin(&list, "Stack");
+	add_node_begin(&list, "Engineer");
+
+	printf("Added to the beginning:\n");
+	print_list(list);
+
+	return (0);
+}
